93f1b4c6f863ddb605190dda5d277df61476b577
kristiyanpetrov <chrustuanpetrow@gmail.com>
2020-05-09 23:24:55 +0100

    fix counter in shopping cart

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {CartService} from '../_services/cart.service';
-import {Products} from '../_models/products.model';
 
 @Component({
   selector: 'app-navbar',
@@ -8,13 +7,11 @@ import {Products} from '../_models/products.model';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  products: Array<Products>;
 
   constructor(private cartService: CartService) {
   }
 
   ngOnInit(): void {
-    this.products = this.cartService.getProducts();
   }
 
 }
