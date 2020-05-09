5efbdcd48784f36b7b2285ce1c8d8a8fdda843d0
kristiyanpetrov <chrustuanpetrow@gmail.com>
2020-05-09 17:54:07 +0100

    added img in assets folder and remove url from mock data for img

--- src/app/mock-data/products.ts
+++ src/app/mock-data/products.ts
@@ -3,132 +3,132 @@ export const products = [
     name: 'iPhone 11 Pro',
     price: 1359,
     description: 'A large phone with one of the best cameras',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/81HSzsIkJdL._AC_SL1500_.jpg'
+    image: '../assets/img/1.jpg'
   },
   {
     name: 'iPhone 11',
     price: 1124,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/3aVudZK'
+    image: '../assets/img/2.jpg'
   },
   {
     name: 'iPhone XS Max',
     price: 899,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/2xj72KT'
+    image: '../assets/img/3.jpg'
   },
   {
     name: 'iPhone 11',
     price: 1124,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/3aVudZK'
+    image: '../assets/img/2.jpg'
   },
   {
     name: 'iPhone XS Max',
     price: 899,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/2xj72KT'
+    image: '../assets/img/3.jpg'
   },
   {
     name: 'iPhone 11 Pro',
     price: 1359,
     description: 'A large phone with one of the best cameras',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/81HSzsIkJdL._AC_SL1500_.jpg'
+    image: '../assets/img/1.jpg'
   },
   {
     name: 'iPhone 11',
     price: 1124,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/3aVudZK'
+    image: '../assets/img/2.jpg'
   },
   {
     name: 'iPhone XS Max',
     price: 899,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/2xj72KT'
+    image: '../assets/img/3.jpg'
   },
   {
     name: 'iPhone 11',
     price: 1124,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/3aVudZK'
+    image: '../assets/img/2.jpg'
   },
   {
     name: 'iPhone XS Max',
     price: 899,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/2xj72KT'
+    image: '../assets/img/3.jpg'
   },
   {
     name: 'iPhone 11',
     price: 1124,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/3aVudZK'
+    image: '../assets/img/2.jpg'
   },
   {
     name: 'iPhone XS Max',
     price: 899,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/2xj72KT'
+    image: '../assets/img/3.jpg'
   },
   {
     name: 'iPhone 11 Pro',
     price: 1359,
     description: 'A large phone with one of the best cameras',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/81HSzsIkJdL._AC_SL1500_.jpg'
+    image: '../assets/img/1.jpg'
   },
   {
     name: 'iPhone 11',
     price: 1124,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/3aVudZK'
+    image: '../assets/img/2.jpg'
   },
   {
     name: 'iPhone XS Max',
     price: 899,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/2xj72KT'
+    image: '../assets/img/3.jpg'
   },
   {
     name: 'iPhone 11 Pro',
     price: 1359,
     description: 'A large phone with one of the best cameras',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/81HSzsIkJdL._AC_SL1500_.jpg'
+    image: '../assets/img/1.jpg'
   },
   {
     name: 'iPhone 11 Pro',
     price: 1359,
     description: 'A large phone with one of the best cameras',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/81HSzsIkJdL._AC_SL1500_.jpg'
+    image: '../assets/img/1.jpg'
   },
   {
     name: 'iPhone XS Max',
     price: 899,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/2xj72KT'
+    image: '../assets/img/3.jpg'
   },
   {
     name: 'iPhone 11',
     price: 1124,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/3aVudZK'
+    image: '../assets/img/2.jpg'
   },
   {
     name: 'iPhone XS Max',
     price: 899,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/2xj72KT'
+    image: '../assets/img/3.jpg'
   },
   {
     name: 'iPhone 11 Pro',
     price: 1359,
     description: 'A large phone with one of the best cameras',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/81HSzsIkJdL._AC_SL1500_.jpg'
+    image: '../assets/img/1.jpg'
   },
   {
     name: 'iPhone XS Max',
     price: 899,
     description: 'A great phone with one of the best batteries',
-    image: 'https://bit.ly/2xj72KT'
+    image: '../assets/img/3.jpg'
   },
 ];
