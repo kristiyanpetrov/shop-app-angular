975f749986c4166aad6e836ce83dc298e7719625
kristiyanpetrov <chrustuanpetrow@gmail.com>
2020-05-13 01:33:57 +0100

    small change in structure and added new fields of mock data

--- src/app/mock-data/products.ts
+++ src/app/mock-data/products.ts
@@ -2,133 +2,189 @@ export const products = [
   {
     name: 'iPhone 11 Pro',
     price: 1359,
-    description: 'A large phone with one of the best cameras',
-    image: '../assets/img/1.jpg'
+    display: '5.8-inch Super Retina XDR OLED display',
+    waterResistant: 'Water and dust resistant (4 metres for up to 30 minutes, IP68)',
+    camera: 'Triple-camera system with 12MP Ultra Wide, Wide, and Telephoto cameras; Night mode, Portrait mode, and 4K video up to 60fps',
+    frontCamera: '12MP TrueDepth front camera with Portrait mode, 4K video, and Slo-Mo',
+    futures: 'Face ID for secure authentication and Apple Pay',
+    processor: 'A13 Bionic chip with third-generation Neural Engine',
+    charging: 'Fast charge with 18W adapter included',
+    image: [
+      {
+        color: 'silver',
+        phoneColor: 'Silver',
+        path: '../assets/img/11pro/11silver.jpg',
+      },
+      {
+        color: 'darkslategrey',
+        phoneColor: 'Space Grey',
+        path: '../assets/img/11pro/11spacegrey.jpg',
+      },
+      {
+        color: 'grey',
+        phoneColor: 'Midnight',
+        path: '../assets/img/11pro/11midnight.jpg',
+      },
+      {
+        color: 'palegoldenrod',
+        phoneColor: 'Gold',
+        path: '../assets/img/11pro/11gold.jpg',
+      }
+    ]
+  },
+  {
+    name: 'iPhone 11 Max',
+    price: 1568,
+    display: '5.8-inch Super Retina XDR OLED display',
+    waterResistant: 'Water and dust resistant (4 metres for up to 30 minutes, IP68)',
+    camera: 'Triple-camera system with 12MP Ultra Wide, Wide, and Telephoto cameras; Night mode, Portrait mode, and 4K video up to 60fps',
+    frontCamera: '12MP TrueDepth front camera with Portrait mode, 4K video, and Slo-Mo',
+    futures: 'Face ID for secure authentication and Apple Pay',
+    processor: 'A13 Bionic chip with third-generation Neural Engine',
+    charging: 'Fast charge with 18W adapter included',
+    image: [
+      {
+        color: 'grey',
+        phoneColor: 'Midnight',
+        path: '../assets/img/11pro/11midnight.jpg',
+      },
+      {
+        color: 'darkslategrey',
+        phoneColor: 'Space Grey',
+        path: '../assets/img/11pro/11spacegrey.jpg',
+      },
+      {
+        color: 'palegoldenrod',
+        phoneColor: 'Gold',
+        path: '../assets/img/11pro/11gold.jpg',
+      },
+      {
+        color: 'silver',
+        phoneColor: 'Silver',
+        path: '../assets/img/11pro/11silver.jpg',
+      }
+    ]
   },
   {
     name: 'iPhone 11',
-    price: 1124,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/2.jpg'
+    price: 1180,
+    display: '5.8-inch Super Retina XDR OLED display',
+    waterResistant: 'Water and dust resistant (4 metres for up to 30 minutes, IP68)',
+    camera: 'Triple-camera system with 12MP Ultra Wide, Wide, and Telephoto cameras; Night mode, Portrait mode, and 4K video up to 60fps',
+    frontCamera: '12MP TrueDepth front camera with Portrait mode, 4K video, and Slo-Mo',
+    futures: 'Face ID for secure authentication and Apple Pay',
+    processor: 'A13 Bionic chip with third-generation Neural Engine',
+    charging: 'Fast charge with 18W adapter included',
+    image: [
+      {
+        phoneColor: 'Yellow',
+        color: 'yellow',
+        path: '../assets/img/11/11yellow.jpg',
+      },
+      {
+        phoneColor: 'Black',
+        color: 'black',
+        path: '../assets/img/11/11black.jpg',
+      },
+      {
+        phoneColor: 'Green',
+        color: 'lightseagreen',
+        path: '../assets/img/11/11green.jpg',
+      },
+      {
+        phoneColor: 'Red',
+        color: 'red',
+        path: '../assets/img/11/11red.jpg',
+      }
+    ]
   },
   {
-    name: 'iPhone XS Max',
-    price: 899,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/3.jpg'
+    name: 'Samsung Galaxy S10',
+    price: 1040,
+    display: '5.8-inch Super Retina XDR OLED display',
+    waterResistant: 'Water and dust resistant (4 metres for up to 30 minutes, IP68)',
+    camera: 'Triple-camera system with 12MP Ultra Wide, Wide, and Telephoto cameras; Night mode, Portrait mode, and 4K video up to 60fps',
+    frontCamera: '12MP TrueDepth front camera with Portrait mode, 4K video, and Slo-Mo',
+    futures: 'Face ID for secure authentication and Apple Pay',
+    processor: 'A13 Bionic chip with third-generation Neural Engine',
+    charging: 'Fast charge with 18W adapter included',
+    image: [
+      {
+        phoneColor: 'White',
+        color: 'white',
+        path: '../assets/img/s10/s10white.jpg'
+      },
+      {
+        phoneColor: 'White',
+        color: 'black',
+        path: '../assets/img/s10/s10black.jpg'
+      },
+      {
+        phoneColor: 'Green',
+        color: 'lightseagreen',
+        path: '../assets/img/s10/s10green.jpg'
+      },
+      {
+        phoneColor: 'Silver',
+        color: 'silver',
+        path: '../assets/img/s10/s10silver.jpg'
+      }
+    ]
   },
   {
-    name: 'iPhone 11',
-    price: 1124,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/2.jpg'
+    name: 'Samsung Galaxy S20',
+    price: 1480,
+    display: '5.8-inch Super Retina XDR OLED display',
+    waterResistant: 'Water and dust resistant (4 metres for up to 30 minutes, IP68)',
+    camera: 'Triple-camera system with 12MP Ultra Wide, Wide, and Telephoto cameras; Night mode, Portrait mode, and 4K video up to 60fps',
+    frontCamera: '12MP TrueDepth front camera with Portrait mode, 4K video, and Slo-Mo',
+    futures: 'Face ID for secure authentication and Apple Pay',
+    processor: 'A13 Bionic chip with third-generation Neural Engine',
+    charging: 'Fast charge with 18W adapter included',
+    image: [
+      {
+        phoneColor: 'Pink',
+        color: 'pink',
+        path: '../assets/img/s20/s20pink.jpg'
+      },
+      {
+        phoneColor: 'Black',
+        color: 'black',
+        path: '../assets/img/s20/s20black.jpg'
+      },
+      {
+        phoneColor: 'Grey',
+        color: 'grey',
+        path: '../assets/img/s20/s20grey.jpg'
+      }
+    ]
   },
   {
-    name: 'iPhone XS Max',
-    price: 899,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/3.jpg'
-  },
-  {
-    name: 'iPhone 11 Pro',
-    price: 1359,
-    description: 'A large phone with one of the best cameras',
-    image: '../assets/img/1.jpg'
-  },
-  {
-    name: 'iPhone 11',
-    price: 1124,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/2.jpg'
-  },
-  {
-    name: 'iPhone XS Max',
-    price: 899,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/3.jpg'
-  },
-  {
-    name: 'iPhone 11',
-    price: 1124,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/2.jpg'
-  },
-  {
-    name: 'iPhone XS Max',
-    price: 899,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/3.jpg'
-  },
-  {
-    name: 'iPhone 11',
-    price: 1124,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/2.jpg'
-  },
-  {
-    name: 'iPhone XS Max',
-    price: 899,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/3.jpg'
-  },
-  {
-    name: 'iPhone 11 Pro',
-    price: 1359,
-    description: 'A large phone with one of the best cameras',
-    image: '../assets/img/1.jpg'
-  },
-  {
-    name: 'iPhone 11',
-    price: 1124,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/2.jpg'
-  },
-  {
-    name: 'iPhone XS Max',
-    price: 899,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/3.jpg'
-  },
-  {
-    name: 'iPhone 11 Pro',
-    price: 1359,
-    description: 'A large phone with one of the best cameras',
-    image: '../assets/img/1.jpg'
-  },
-  {
-    name: 'iPhone 11 Pro',
-    price: 1359,
-    description: 'A large phone with one of the best cameras',
-    image: '../assets/img/1.jpg'
-  },
-  {
-    name: 'iPhone XS Max',
-    price: 899,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/3.jpg'
-  },
-  {
-    name: 'iPhone 11',
-    price: 1124,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/2.jpg'
-  },
-  {
-    name: 'iPhone XS Max',
-    price: 899,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/3.jpg'
-  },
-  {
-    name: 'iPhone 11 Pro',
-    price: 1359,
-    description: 'A large phone with one of the best cameras',
-    image: '../assets/img/1.jpg'
-  },
-  {
-    name: 'iPhone XS Max',
-    price: 899,
-    description: 'A great phone with one of the best batteries',
-    image: '../assets/img/3.jpg'
+    name: 'iPhone XS',
+    price: 920,
+    display: '5.8-inch Super Retina XDR OLED display',
+    waterResistant: 'Water and dust resistant (4 metres for up to 30 minutes, IP68)',
+    camera: 'Triple-camera system with 12MP Ultra Wide, Wide, and Telephoto cameras; Night mode, Portrait mode, and 4K video up to 60fps',
+    frontCamera: '12MP TrueDepth front camera with Portrait mode, 4K video, and Slo-Mo',
+    futures: 'Face ID for secure authentication and Apple Pay',
+    processor: 'A13 Bionic chip with third-generation Neural Engine',
+    charging: 'Fast charge with 18W adapter included',
+    image: [
+      {
+        phoneColor: 'Gold',
+        color: 'palegoldenrod',
+        path: '../assets/img/xs/xsgold.jpg'
+      },
+      {
+        phoneColor: 'Black',
+        color: 'black',
+        path: '../assets/img/xs/xsblack.jpg'
+      },
+      {
+        phoneColor: 'White',
+        color: 'white',
+        path: '../assets/img/xs/xswhite.jpg'
+      },
+    ]
   },
 ];
