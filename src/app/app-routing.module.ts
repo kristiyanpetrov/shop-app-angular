9694abefe27946cfb0123429c7141ae7a16755a5
kristiyanpetrov <chrustuanpetrow@gmail.com>
2020-05-28 00:59:10 +0100

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductDetailsComponent} from './product-details/product-details.component';
 import {CartComponent} from './cart/cart.component';
+import {AdminComponent} from './admin/admin.component';
+import {ProductsComponent} from './admin/products/products.component';
+import {EditProductComponent} from './admin/edit-product/edit-product.component';
 
 
 const routes: Routes = [
   {path: '', component: ProductListComponent},
-  {path: 'products/:productId', component: ProductDetailsComponent},
-  {path: 'cart', component: CartComponent}
+  // {path: 'products/:productId', component: ProductDetailsComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'admin/products', component: ProductsComponent},
+  {path: 'admin/products/edit/:productId', component: EditProductComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
